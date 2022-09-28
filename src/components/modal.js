b81d11605ccd169d786286a5a0f1ecce87315b0f
Gupta Yogesh <Y.Gupta@nexionindia.com>
2022-09-28 18:23:52 +0530

    modal works

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,7 +4,17 @@ class Modal extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      efname: "",
+      eage: "",
+      ecity: "",
+      ecountry: "",
+    };
   }
+
+  editOnChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
 
   render() {
     return (
@@ -14,9 +24,43 @@ class Modal extends Component {
         }
       >
         <section className="modal-main">
-          <h4 key={this.props.children.id}>{this.props.children.fname}</h4>
+          {/* <h4 key={this.props.children.id}>{this.props.children.fname}</h4>
           <h4>{this.props.children.city}</h4>
-          <h4>{this.props.children.age}</h4>
+          <h4>{this.props.children.age}</h4> */}
+
+          {/* <form onSubmit={(e) => this.formHandler(e)}> */}
+          <form>
+            {/* <form action="http://localhost:5000/formPost/" method="post"> */}
+            <input
+              type="text"
+              placeholder="Name"
+              name="fname"
+              value={this.props.children.fname}
+              onChange={(e) => this.editOnChange(e)}
+            />
+            <input
+              type="number"
+              placeholder="Age"
+              name="age"
+              value={this.props.children.age}
+              onChange={(e) => this.editOnChange(e)}
+            />
+            <input
+              type="text"
+              placeholder="City"
+              name="city"
+              value={this.props.children.city}
+              onChange={(e) => this.editOnChange(e)}
+            />
+            <input
+              type="text"
+              placeholder="Country"
+              name="country"
+              value={this.props.children.country}
+              onChange={(e) => this.editOnChange(e)}
+            />
+          </form>
+
           <button
             type="button"
             style={{ backgroundColor: "green", color: "white" }}
